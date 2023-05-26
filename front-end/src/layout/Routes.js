3a8ed0a715914965b651f86ecaedaec74f85f264
Jenni Grubbs <jennigrubbsjrn@gmail.com>
2023-05-25 22:08:07 -0600

    Committing updates to more front-end files.

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import CreateReservation from "../components/reservations/CreateReservation";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "../dashboard/Dashboard";
+import Dashboard from "../components/dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import CreateTable from "../components/tables/CreateTable";
+import SeatReservation from "../components/reservations/SeatReservation";
+import Search from "../components/search/Search";
+import EditReservation from "../components/reservations/EditReservation";
 
 /**
  * Defines all the routes for the application.
@@ -12,14 +16,29 @@ import { today } from "../utils/date-time";
  *
  * @returns {JSX.Element}
  */
-function Routes() {
+ function Routes() {
   return (
     <Switch>
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route path="/reservations/new">
+        <CreateReservation />
+      </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+      <Route path="/tables/new">
+        <CreateTable />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
